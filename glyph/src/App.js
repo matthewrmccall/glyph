66adfa2e90ec324421a2a05b32d6eac2b6ac629c
matthewrmccall <matt@mrmccall.com>
2020-11-17 17:14:56 -0500

    added a form

--- glyph/src/App.js
+++ glyph/src/App.js
@@ -1,26 +1,22 @@
 //glyph app for creating FOIA requests
-import logo from './logo.png';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Start your FOIA request!</h1>
+      <form>
+      <fieldset>
+        <label>
+          <p>Your Name</p>
+          <input name="name" />
+        </label>
+      </fieldset>
+      <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
