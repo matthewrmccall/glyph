ef7d766f37ee7de2547d1491ce527535d08b1067
matthewrmccall <matt@mrmccall.com>
2020-11-18 10:47:45 -0500

    added function to handle change and used it in onChange event handler

--- glyph/src/App.js
+++ glyph/src/App.js
@@ -3,11 +3,12 @@
 import React, { useReducer, useState} from 'react';
 import './App.css';
 
-//function to pull name and value from event.target
+// reducer function that takes inputs state and event
 const formReducer = (state, event) => {
   return {
     ...state,
-    [event.target.name]: event.target.value
+    // pass an event object with properties name and value
+    [event.name]: event.value
   }
 }
 
@@ -18,24 +19,34 @@ function App() {
   const handleSubmit = event => {
     event.preventDefault();
     setSubmitting(true);
-
+// show Submitting Form message for 3 sec on button click
     setTimeout(() => {
       setSubmitting(false);
     }, 3000)
   }
-
+// function to pull the data from the event.target
+// and pass the object to setFormData
+  const handleChange = event => {
+    // setFormData is an object with keys: name and value
+    // and values of event.target.name and event.target.value
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+// the user-facing section
+// update the onChange event handler to use the handleChange function
   return (
     <div className="wrapper">
       <h1>Let's get started!</h1>
       {submitting &&
       <div>Submitting Form...</div>
       }
-      // connect to JSX using an event handler
       <form onSubmit={handleSubmit}>
       <fieldset>
         <label>
           <p>Your Name</p>
-          <input name="name" onChange={setFormData}/>
+          <input name="name" onChange={handleChange}/>
         </label>
       </fieldset>
       <button type="submit">Submit</button>
