71690862e9006d8197e92e4bc425d86ca2032aab
matthewrmccall <matt@mrmccall.com>
2020-11-20 16:32:58 -0500

    updated form fields

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+// glyph is a signup form for veterans disability compensation help
 // import React and use hooks to create objects
 import React, { useReducer, useState} from 'react';
 import './App.css';
 
 // reducer function that takes inputs state and event
 const formReducer = (state, event) => {
+  if(event.reset) {}
   return {
     ...state,
     // pass an event object with properties name and value
@@ -12,7 +14,7 @@ const formReducer = (state, event) => {
 }
 
 function App() {
-  const [formData, setFormData] = useReducer(formReducer, {});
+  const [formData, setFormData] = useReducer(formReducer, { count: 100 });
   const [submitting, setSubmitting] = useState(false);
 
   const handleSubmit = event => {
@@ -21,23 +23,28 @@ function App() {
 // show Submitting Form message for 3 sec on button click
     setTimeout(() => {
       setSubmitting(false);
-    }, 3000)
+    }, 6000)
   }
 // function to pull the data from the event.target
 // and pass the object to setFormData
   const handleChange = event => {
+    // check if event.target.type is checkbox
+    const isCheckbox = event.target.type === 'checkbox';
     // setFormData is an object with keys: name and value
     // and values of event.target.name and event.target.value
     setFormData({
       name: event.target.name,
-      value: event.target.value,
+      // if type is checkbox, pass event.target.checked as the value
+      // otherwise, pass event.target.value as value
+      value: isCheckbox ? event.target.checked : event.target.value
     });
   }
 // the user-facing section
 // update the onChange event handler to use the handleChange function
   return (
     <div className="wrapper">
-      <h1>Let's get started!</h1>
+      <h1>Get help with your VA Claim</h1>
+      <h2>Let's increase your VA rating</h2>
       {submitting &&
       <div>
         You are submitting the following:
@@ -48,32 +55,40 @@ function App() {
           </ul>
       </div>
       }
+
       <form onSubmit={handleSubmit}>
       <fieldset>
         <label>
-          <p>Your Name</p>
-          <input name="name" onChange={handleChange}/>
+          <p>Name</p>
+          <input name="name" onChange={handleChange} value={formData.name}/>
+          <p>Email</p>
+          <input name="email" onChange={handleChange}/>
+          <p>Phone (recommended)</p>
+          <input name="phone" onChange={handleChange}/>
         </label>
       </fieldset>
+
       <fieldset>
         <label>
-          <p>How can we help?</p>
-          <select name="claim" onChange={handleChange}>
-            <option value="">--Please choose an option--</option>
-            <option value="New Claim">New Claim</option>
-            <option value="Existing Claim">Existing Claim</option>
-            <option value="Appeal">Appeal</option>
+          <p>Please choose an option:</p>
+          <select name="type" onChange={handleChange} value={formData.type}>
+            <option value="VA Claim">VA Claim</option>
+            <option value="VA Appeal">VA Appeal</option>
+            <option value="I'm not sure">I'm not sure</option>
           </select>
         </label>
+
         <label>
-        <p>Count</p>
-        <input type="number" name="count" onChange={handleChange} step="1"/>
+        <p>Input field is count</p>
+        <input type="number" name="count" onChange={handleChange} step="1" value={formData.count}/>
         </label>
+
         <label>
-          <p>I agree to the terms of service.</p>
-          <input type="checkbox" name="tos" onChange={handleChange} />
+          <p>Contact me about my VA Claim/Appeal.</p>
+          <input type="checkbox" name="contact" onChange={handleChange} checked={formData['contact']} />
         </label>
       </fieldset>
+
       <button type="submit">Submit</button>
       </form>
     </div>
